565deddb2aac4cc97e1004e557573425dc4450ce
rebeloper <therebeloper@gmail.com>
2023-02-21 17:31:59 +0200

    api

---
+++ Sources/DiscordAPI/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  
+//
+//  Created by Alex Nagy on 21.02.2023.
+//
+
+import Foundation
+
+struct API {
+    static func hello() {
+        print("Hello, world!")
+    }
+}
--- Tests/DiscordAPITests/DiscordAPITests.swift
+++ Tests/DiscordAPITests/DiscordAPITests.swift
@@ -6,6 +6,5 @@ final class DiscordAPITests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(DiscordAPI().text, "Hello, World!")
     }
 }
