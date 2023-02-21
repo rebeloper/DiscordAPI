f892fdc93d8bb928f429e08b91541cf78a2d65f1
rebeloper <therebeloper@gmail.com>
2023-02-21 18:03:21 +0200

    added framework

---
+++ Sources/DiscordAPI.xcframework/macos-arm64_x86_64/DiscordAPI.framework/Versions/A/Headers/DiscordAPI.h
@@ -0,0 +1,18 @@
+//
+//  DiscordAPI.h
+//  DiscordAPI
+//
+//  Created by Alex Nagy on 21.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DiscordAPI.
+FOUNDATION_EXPORT double DiscordAPIVersionNumber;
+
+//! Project version string for DiscordAPI.
+FOUNDATION_EXPORT const unsigned char DiscordAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiscordAPI/PublicHeader.h>
+
+
